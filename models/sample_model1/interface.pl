ccaca43cf2a64a5faa18e2380954f58358c0c457
Christian Theil Have <cth@mox.ruc.dk>
2010-03-05 10:39:02 +0100

    Error checking of sample_model1

--- models/sample_model1/interface.pl
+++ models/sample_model1/interface.pl
@@ -6,7 +6,13 @@ lost_best_annotation(ParamFile,[InputFile],OutputFile) :-
 	write('sample model 1: '),nl,
 	write(lost_best_annotation(ParamFile,[InputFile],OutputFile)),nl,
 	prism(sample1), % Load the actual PRISM model
+	write(restore_sw(ParamFile)),nl, % Restore switch values
 	restore_sw(ParamFile), % Restore switch values
 	load_sequence_list_from_file(InputFile,InputSequence), % Load the input sequence
-	viterbig(sample1(InputSequence,OutputSequence)), % Derive an annotation somehow
+        % Derive an annotation somehow
+        write(viterbig(sample1(InputSequence,OutputSequence))),nl,
+	check_or_fail(viterbig(sample1(InputSequence,OutputSequence)), 
+                        error(viterbig_says_no_no_no)),
+        write('model 1 produced output sequence: '),
+        write(OutputSequence),nl,
 	save_sequence_list_to_file(OutputFile,OutputSequence). % Save result to filename given
