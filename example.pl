90ce5a0933ec3064cdde0d067b637b9b1a86b1b1
petit <petit@komm-nt502.(none)>
2010-03-09 11:37:35 +0100

    Example Easygene

--- example.pl
+++ example.pl
@@ -22,3 +22,12 @@ test :-
 	write('Resulting annotation sequence:'),nl,
 	write(AnnotSeq),nl.
 
+
+
+test_easygene_parser :-
+        %lost_sequence_file('eg_U00096.dat'),
+        get_annotation_file(parser_easygene,
+                            _,
+                            ['eg_U00096.dat'],
+                            AnnotFile),
+        write(AnnotFile)
