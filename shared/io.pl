63fabf0b4910b2864bc8190150a44759dc9c2e38
petit <petit@komm-nt502.(none)>
2010-03-09 18:50:39 +0100

    Loading from DB (to test and debug) but compiled

--- shared/io.pl
+++ shared/io.pl
@@ -8,7 +8,7 @@
 % AUTHORS: Lost Members
 %
 % FUNCTION :
-%      Manipulation of files
+%      Manipulation of files: loading information from file, saving file from information
 % 
 % HISTORIC :
 %  09/03: creation of file         MP
@@ -49,7 +49,7 @@ sequence_terms_to_data_elements([elem(_,Data)|R1],[Data|R2]) :-
 %
 %%%%%%%%%%%%%%%%%%%%%%%%
 % Type_Info: sequence
-% Terms  in File are composed of List of data.
+% Terms in File are composed of List of data.
 % Annotation is a list
 % Options available: Options = [data_position(Position),
 %                               range(Min,Max)]
@@ -62,11 +62,16 @@ load_annotation_from_file(sequence,Options,File,Annotation) :-
         sequence_terms_to_annotations(Options,SortedTerms,Annotation).
 
 
-
+%%%%%%%%%%%%%%%%%%%%
 % Type: db
+% Terms in File are composed of Range that delimites a specific region.
 % Options available: Options = [in_db(Letter),not_in_db(Letter),
-%                               data_position(Position),range(Min,Max)]
-
+%                               range_position(Position),range(Min,Max)]
+% Note: assumption is done on the format of the database. Information is extracted from a list
+% of terms that have a parameter Range to describe a specific region of the genome.
+% By defaut, annotation of the specific region is 1 and 0 when the region is not specific.
+%
+%%%%%%%%%%%%%%%%%%%%%
 load_annotation_from_file(db,Options,File,Annotation) :-
         terms_from_file(File,Terms),
         % Technically not necessary since they will be sorted if this
@@ -74,6 +79,10 @@ load_annotation_from_file(db,Options,File,Annotation) :-
 	sort('=<',Terms,SortedTerms), 
         db_terms_to_annotations(Options,SortedTerms,Annotation).
 
+
+
+
+% Utils for load_annotation_from_file(sequence,....)
 
 %%%
 % sequence_terms_annotations(++Options,++List_Terms,--Annotation)
@@ -97,7 +106,7 @@ sequence_terms_to_annotations(_Options,[],[]) :-
 % Options Range and Data Position
 sequence_terms_to_annotations(Options,[Data|Data_Terms],Annotation) :-
         member(range(Min,Max),Options),
-        member(data_position(Data_Position),Options),
+        member(range_position(Data_Position),Options),
         !,
         Data =.. [_Functor|Rest_Data],
         nth1(Data_Position,Rest_Data,Sequence_Data),
@@ -114,7 +123,7 @@ sequence_terms_to_annotations(Options,[Data|Data_Terms],Annotation) :-
 
 % Options Data Position only
 sequence_terms_to_annotations(Options,[Data|Data_Terms],Annotation) :-
-        member(data_position(Data_Position),Options),
+        member(range_position(Data_Position),Options),
         !,
         Data =.. [_Functor|Rest_Data],
         nth1(Data_Position,Rest_Data,Sequence_Data),
@@ -161,14 +170,14 @@ sequence_terms_to_annotations_rec([Annot|Rest_Sequence_Data],range(Min,Max),Posi
 
 
 
+
+
+% Utils for load_annotation_from_file(sequence)
+
 %%%
 % db_terms_annotations(++Options,++List_Terms,--Annotation)
 %%%
 %Options = [in_db(Letter),out_db(Letter),range_position(Param_Start,Param_End),range(Min,Max)]
-% Note: assumption is done on the format of the database. Information is extracted from a list
-% of terms that have a parameter Range to describe a specific region of the genome.
-% By defaut, annotation of the specific region is 1 and 0 when the region is not specific.
-
 
 
 db_terms_to_annotations(_Options,[],[]) :-
@@ -180,18 +189,108 @@ db_terms_to_annotations(Options,[DB|List_Terms],Annotation) :-
         !,
         init_db_terms(Options,Annot_Format,Range_Position),
         get_next_range(Range_Position,DB,Range),
-        db_terms_to_annotations_rec(Options,1,range(Min,Max),Range,Annot_Format,Range_Position,List_Terms,Annotation).
+        db_terms_to_annotations_rec(1,range(Min,Max),Range,Annot_Format,Range_Position,List_Terms,Annotation).
 
 
 
 db_terms_to_annotations(Options,[DB|List_Terms],Annotation) :-
         init_db_terms(Options,Annot_Format,Range_Position),
         get_next_range(Range_Position,DB,Range),
-        db_terms_to_annotations_rec(Options,Range,Annot_Format,Range_Position,List_Terms,Annotation).
+        db_terms_to_annotations_rec(1,_,Range,Annot_Format,Range_Position,List_Terms,Annotation).
 
 
 % Recursive call
 % Options: No range(Min,Max) 
+
+% When no range is specificied, annotation stops when the last Position = Max for the last Range of the db
+db_terms_to_annotations_rec(Position,Var,(_,Max),_Annot_Format,_Range_Position,[],[]) :-
+        var(Var),
+        Position > Max,
+        !.
+
+% End of a Range, 
+db_terms_to_annotations_rec(Position,Var,(_,Max),Annot_Format,Range_Position,[DB|List_Terms],Annotations) :-
+        var(Var),
+        Position > Max,
+        !,
+        get_next_range(Range_Position,DB,New_Range),
+        db_terms_to_annotations_rec(Position,Var,New_Range,Annot_Format,Range_Position,List_Terms,Annotations).
+
+% Outside a specific Region 
+db_terms_to_annotations_rec(Position,Var,(Min,_Max),(Letter_Out,_Letter_In),Range_Position,List_Terms,[Letter_Out|Annotations]) :-
+        var(Var),
+        Position < Min,
+        !,
+        Next_Position is Position+1,
+        db_terms_to_annotations_rec(Next_Position,Var,(Min,_Max),(Letter_Out,_Letter_In),Range_Position,List_Terms,Annotations).
+
+
+% Inside a specific Region 
+db_terms_to_annotations_rec(Position,Var,(Min,Max),(_Letter_Out,Letter_In),Range_Position,List_Terms,[Letter_In|Annotations]) :-
+        var(Var),
+        Position >= Min,
+        Position >= Max,
+        !,
+        Next_Position is Position+1,
+        db_terms_to_annotations_rec(Next_Position,Var,(Min,_Max),(_Letter_Out,_Letter_In),Range_Position,List_Terms,Annotations).
+
+
+
+
+% Recursive call
+% Options: range(Range_Min,Range_Max) 
+
+% Ends when Position = Max_Range
+db_terms_to_annotations_rec(Position,range(_Range_Min,Range_Max),_Range,_Annot_Format,_Range_Position,_List_Terms,[]) :-
+        Position > Range_Max,
+        !.
+
+
+% End of the list of terms, but inside the specified range
+db_terms_to_annotations_rec(Position,range(Range_Min,Range_Max),(_Min,Max),(Letter_Out,_Letter_In),_Range_Position,[],[Letter_Out|Annotations]) :-
+        Position > Max,
+        Position >= Range_Min,
+        Position =< Range_Max,
+        !,
+        New_Position is Position+1,
+        db_terms_to_annotations_rec(New_Position,range(Range_Min,Range_Max),(_Min,Max),(Letter_Out,_Letter_In),_Range_Position,[],Annotations).
+
+
+% End of a DB range update by a new one
+db_terms_to_annotations_rec(Position,range(Range_Min,Range_Max),(_Min,Max),Annot_Format,Range_Position,[DB|List_Terms],Annotations) :-
+        Position > Max,
+        !,
+        get_next_range(Range_Position,DB,New_Range),
+        db_terms_to_annotations_rec(Position,range(Range_Min,Range_Max),New_Range,Annot_Format,Range_Position,List_Terms,Annotations).
+
+
+% No generation: outside the specified range + update of the new DB range
+db_terms_to_annotations_rec(Position,range(Range_Min,Range_Max),(Min,Max),_Annot_Format,_Range_Position,_List_Terms,Annotations) :-
+        Position < Range_Min,
+        !,
+        update_position_jump(Range_Min,(Min,Max),New_Position),
+        db_terms_to_annotations_rec(New_Position,range(Range_Min,Range_Max),(Min,Max),_Annot_Format,_Range_Position,_List_Terms,Annotations).
+
+
+% Inside the specified range but outside a specific region
+db_terms_to_annotations_rec(Position,range(Range_Min,Range_Max),(Min,_Max),(Letter_Out,_Letter_In),_Range_Position,_List_Terms,[Letter_Out|Annotations]) :-
+        Position >= Range_Min,
+        Position =< Range_Max,
+        Position < Min,
+        !,
+        New_Position is Position+1,
+        db_terms_to_annotations_rec(New_Position,range(Range_Min,Range_Max),(Min,_Max),(Letter_Out,_Letter_In),_Range_Position,_List_Terms,Annotations).
+
+
+% Inside the specified range but inside a specific region
+db_terms_to_annotations_rec(Position,range(Range_Min,Range_Max),(Min,Max),(_Letter_Out,Letter_In),_Range_Position,_List_Terms,[Letter_In|Annotations]) :-
+        Position >= Range_Min,
+        Position =< Range_Max,
+        Position >= Min,
+        Position =< Max,
+        !,
+        New_Position is Position+1,
+        db_terms_to_annotations_rec(New_Position,range(Range_Min,Range_Max),(Min,_Max),(_Letter_Out,Letter_In),_Range_Position,_List_Terms,Annotations).
 
 
 %%%
@@ -225,10 +324,23 @@ get_next_range(Param_Start,Param_End,DB,(Min,Max)) :-
         nth1(Param_End,List_Params,Max).
 
 
+% update_position_jump(++Range_Min,(++Min,++Max),-Position)
+update_position_jump(Range_Min,(_Min,Max),New_Position) :-
+        Range_Min < Max,
+        !,
+        New_Position = Range_Min.
 
-%-------
-% Saving information from file
-%-------
+
+% update_position_jump(++Range_Min,(++Min,++Max),-Position)
+update_position_jump(_Range_Min,(_Min,Max),New_Position) :-
+        New_Position is Max+1.
+
+
+
+
+%--------------------------------
+% Saving information from file  %
+%--------------------------------
 
 % save_sequence_list_to_file(++File,--Sequence)
 
