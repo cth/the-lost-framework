4d2301723735df937240eded2c2ae7635fd24ec4
Ole <otl@OTL-tablet.(none)>
2010-03-10 13:15:25 +0100

    .... and yet another

--- consorf.pl
+++ consorf.pl
@@ -13,22 +13,21 @@ test_consorf :-
 	lost_sequence_file('u00096-20k_orf_+1',InputOrfFile), % ,ConsFile),
 
 	% Parameter id "sample2" resolves to models/consorf_genefinder/parameters/consorf_genefinder.prb
-	lost_model_parameter_file(consorf_genefinder, consorf_genefinder, ParameterFile)
-	
-	/*
-	,
+	lost_model_parameter_file(consorf_genefinder, consorf_genefinder, ParameterFile),
 	get_annotation_file(consorf_genefinder,  					% Name of model (resolves to models/sample_model2/)
 			    [InputOrfFile,InputConsFile], 						% A list of input files
 			    [option(parameter_file,ParameterFile)],   % Extra options
-			    AnnotFile),     														% AnnotFile is unified to the name of the file that  annotation is written to
+			    _AnnotFile)     														% AnnotFile is unified to the name of the file that  annotation is written to
 
 																										% Load the sequence (AnnotSeq) contained in the file AnnotFile 
 	
+	/*,
 	load_annotation_from_file(AnnotFile,AnnotSeq),
 	writeln('here i am'),
 	
 	write('Resulting annotation sequence:'),nl,
 	write(AnnotSeq),nl
-	*/.
+	*/
+	.
 
 
