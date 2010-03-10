478e360476a7af966d4b01d7703ac5082d2e9458
Ole <otl@OTL-tablet.(none)>
2010-03-10 11:40:29 +0100

    .... and yet another

--- models/consorf_genefinder/interface.pl
+++ models/consorf_genefinder/interface.pl
@@ -15,31 +15,31 @@ lost_best_annotation([OrfFile,ConsFile],Options,OutputFile) :-
 				write(restore_sw(ParamFile)),nl, % Restore switch values                              
 	
 	restore_sw(ParamFile), % Restore switch values 
-				write('parameters loaded'),nl,                           
+	write('parameters loaded'),nl,                           
 	
 	write(load_annotation_from_file(sequence,[data_position(6),range(1,48)],OrfFile,InputOrf)),nl, % Load the input sequence      
   			load_annotation_from_file(sequence,[data_position(6),range(1,48)],OrfFile,InputOrf),
-  			write('OrfFile loaded'),nl,
-  load_annotation_from_file(sequence,[data_position(6),range(1,48)],ConsFile,InputCons),      
-        write('ConsFile loaded'),nl,
+  write('OrfFile loaded'),nl,
+  			load_annotation_from_file(sequence,[data_position(6),range(1,48)],ConsFile,InputCons),      
+  write('ConsFile loaded'),nl,
         % Derive an annotation somehow
                                                           
-  			write(viterbiAnnot(consorf(InputOrf,InputCons,OutputAnnotation))),nl,                      
+  write(viterbiAnnot(consorf(InputOrf,InputCons,OutputAnnotation1))),nl,                      
 	
-	check_or_fail(viterbiAnnot(consorf(InputOrf,InputCons,OutputAnnotation),_),                        
+				check_or_fail(viterbiAnnot(consorf(InputOrf,InputCons,OutputAnnotation),_),                        
                         error(viterbiAnnot_says_no_no_no)),                                 
   
-        write('LoSt consorf genefinder produced output prediction: '),                                    
-        write(OutputAnnotation),nl,                                                       
+  write('LoSt consorf genefinder produced output prediction: '),                                    
+  write(OutputAnnotation),nl,                                                       
 				
-				
+	/*			
 	write(save_sequence_list_to_file(OutputFile,OutputAnnotation)),nl,                      
 	save_sequence_list_to_file(OutputFile,OutputAnnotation), % Save result to filename given
+	*/
 	
-	/*
 	open(OutputFile,write,OutStream),
 	write('prediction('), write(OutputAnnotation), writeln(').'),
 	close(OutStream),
-	*/
+	
 	
 	write('LoSt consorf genefinder terminated successfully.'),nl.                                         
