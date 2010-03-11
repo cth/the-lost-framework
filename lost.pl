be8388749b3d4e715a39a319f3657b7895538e86
Christian Theil Have <cth@mox.ruc.dk>
2010-03-11 16:45:36 +0100

    Changed lost file to warn about unconfigured lost_base_directory

--- lost.pl
+++ lost.pl
@@ -2,7 +2,11 @@
 % Configuration
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 lost_config(prism_command,'/usr/local/bin/prism ').
-lost_config(lost_base_directory, '/home/cteilhave/temp/lost/').
+lost_config(lost_base_directory,'/change/to/your/local/lost/directory').
+
+lost_include_api(_) :-
+        lost_config(lost_base_directory,'/change/to/your/local/lost/directory'),
+        throw('Please set lost_base_directory!!!').
 
 % Basic rule to glue in other APIs
 lost_include_api(Name) :-
