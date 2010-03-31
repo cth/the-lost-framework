3086fbc8240afed81bbc623eeec19cd0da56962a
petit <petit@komm-nt502.(none)>
2010-03-31 15:23:26 +0200

    Parser XML report from blast into Prolog fact (tested not in the framework)

---
+++ models/parser_blastxml/interface.pl
@@ -0,0 +1,15 @@
+:- ['../../lost.pl'].
+:- lost_include_api(interface). 
+:- lost_include(xml).
+
+% Generate a file of predicate based on Easygene report.
+
+lost_best_annotation([XML_File],Options,OutputFile) :-
+        xml2pl(XML_File,'blast_pl.pl'),
+        parser_blastxml('blast_pl.pl',OutputFile),
+        lost_config(platform,Platform)
+        (Platform = 'windows' ->
+            system('del blast_pl.pl')
+        ;
+            system('rm blast_pl.pl')
+        ).
