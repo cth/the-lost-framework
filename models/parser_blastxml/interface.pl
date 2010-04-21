b8f5070448a0d5b975bcd469e8ec0b7f998116eb
Matthieu Petit <petit@ruc.dk>
2010-04-21 11:12:49 +0200

    Parser typed

--- models/parser_blastxml/interface.pl
+++ models/parser_blastxml/interface.pl
@@ -2,15 +2,16 @@
 :- lost_include_api(interface). 
 :- lost_include_api(xml).
 
-% Generate a file of predicate based on Easygene report.
+% Generate a file of predicate based on Blast XML report.
+
+	    
+% Input Format Specification
+lost_input_formats(lost_best_annotation,[text(xml(_))]).
+% Output Format Specification
+lost_output_format(lost_best_annotation,_,[text(prolog(blast_hit))]).
+
 
 lost_best_annotation([XML_File],Options,OutputFile) :-
         consult('parser_blastxml.pl'),
         xml2pl(XML_File,'blast_pl.pl'),
         parser_blastxml('blast_pl.pl',OutputFile).
-%%%         lost_config(platform,Platform)
-%%%         (Platform = 'windows' ->
-%%%             system('del blast_pl.pl')
-%%%         ;
-%%%             system('rm blast_pl.pl')
-%%%         ).
