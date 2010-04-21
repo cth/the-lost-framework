f53f4c402f7bd9c0033e050870e85bee79049647
Christian Theil Have <cth@wlan-pns-13-115.ruc.dk>
2010-04-21 16:25:55 +0200

    format converter model

---
+++ models/format_converter/interface.pl
@@ -0,0 +1,57 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Converts between different commonly used file-formats
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+:- ['../../lost.pl'].
+:- lost_include_api(interface).
+:- lost_include_api(fasta).
+:- lost_include_api(io).
+
+lost_option(lost_best_annotation,input_format,text(_),'Used to specify the format of the input file.').
+lost_option(lost_best_annotation,output_format,text(_),'Used to specify the format of the output file.').
+lost_option(lost_best_annotation,sequence_index, 1, 'Used to specify the index of a particular sequence in a multisequence format.').
+	    
+% Some ideas for specification of input & output formats...
+lost_input_formats(lost_best_annotation, [text(_)]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% lost_output_format/3 declarations
+% Specify an output format predicate for each input_format, output_format pair
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% fasta (single sequence)  ==> text(prolog(sequence(_)))
+lost_output_format(lost_best_annotation, Options, text(prolog(range(SequenceType)))) :-
+	get_option(Options,sequence_index,_),
+	get_option(Options,input_format,text(fasta_single(SequenceType))),
+	get_option(Options,output_format,text(prolog(sequence(SequenceType)))).
+
+% text(prolog(range)) ==> fasta (single sequence)
+lost_output_format(lost_best_annotation, Options, text(prolog(range(SequenceType)))) :-
+	get_option(Options,output_format,text(prolog(range(SequenceType)))),
+	get_option(Options,input_format,text(fasta_single(SequenceType))).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Main goal implementation rules
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% text(fasta(_)) ==> text(prolog(sequence(_))) 
+lost_best_annotation([InputFile], Options, OutputFile) :-
+	get_option(Options,input_format,text(fasta(_))),
+	get_option(Options,output_format,OutputFormat),
+	get_option(Options,sequence_index,SeqId),
+	% Load the fasta sequence:
+	fasta_load_sequence(InputFile,SeqId,FastaHeaderLine,Sequence),
+	atom_codes(HeaderLineAtom,FastaHeaderLine),
+	% Save it in text(prolog(sequence(_))) format
+	save_annotation_to_sequence_file(HeaderLineAtom, 32, Sequence, OutputFile).
+
+% text(prolog(sequence(_)) ==> text(fasta(_))
+lost_best_annotation([InputFile], Options, OutputFile) :-
+	get_option(Options,input_format,text(prolog(sequence(_)))),
+	get_option(Options,output_format,text(fasta(_))),
+	% Load the sequence from a file:
+	load_annotation_from_file(sequence,[data_position(4)],InputFile,Data),
+	% Save it in text(fasta(_)) format:
+	atom_concat(' generated from file: ', InputFile, HeaderLine),
+	fasta_save_sequence(OutputFile,Data,HeaderLine).
