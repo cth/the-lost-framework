110789a695f29e01557d730262a6d7564f7e6d90
Christian Theil Have <cth@wlan-pns-13-115.ruc.dk>
2010-04-26 13:48:33 +0200

    modify genemark config to work in multiple places

--- models/genemark/config.pl
+++ models/genemark/config.pl
@@ -1,5 +1,5 @@
 % Edit this file to setup up the the path to 
 % the genemark genefinder installation
 
-genemark_config(genemark_command, '/opt/genefinders/genemark_suite_linux_64/gm').
-genemark_config(genemark_parameters_dir, '/opt/genefinders/genemark_suite_linux_64/parameters/').
+genemark_config(genemark_command, '/opt/genefinders/genemark_suite/gm').
+genemark_config(genemark_parameters_dir, '/opt/genefinders/genemark_suite/parameters/').
