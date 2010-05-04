0f5b2569fc7477048af2f1dec76a2c5024009f0a
Christian Theil Have <cth@cbit-pc174.ruc.dk>
2010-05-04 14:25:36 +0200

    gene_filter update

--- scripts/filter_genes.pl
+++ scripts/filter_genes.pl
@@ -1,8 +1,7 @@
 :- ['../lost.pl'].
 :- lost_include_api(interface).
 
-parser_ptt(Name_PTT_File,ParsedPTTFile) :-
-        lost_sequence_file(Name_PTT_File,PTT_File),
+parser_ptt(PTT_File,ParsedPTTFile) :-
         get_annotation_file(parser_ptt, % Name of model
                             [PTT_File], % A list of Input Files
                             [],          % Options
@@ -11,18 +10,24 @@ parser_ptt(Name_PTT_File,ParsedPTTFile) :-
         write(ParsedPTTFile),
 	nl.
 
-filter_genes(ParsedPTT,Filtered) :-
-	lost_sequence_file('U00096',RawGenome),
+filter_genes(ParsedPTT,RawGenome,Filtered) :-
 	get_annotation_file(gene_filter,
 	[ParsedPTT,RawGenome],
 	[	
-		match_extra_fields([gene_name('^y.*$')])
+
 	],
+%		match_extra_fields([gene_name('^z.*$')])			    
 	Filtered).
 
 t :-
-	parser_ptt('U00096_ptt',PTTParsed),
-	filter_genes(PTTParsed,Filtered),
+%	lost_sequence_file('short',RawGenome),
+%        lost_sequence_file('short_ptt',PTT_File),
+
+	lost_sequence_file('U00096',RawGenome),
+        lost_sequence_file('U00096_ptt',PTT_File),
+	
+	parser_ptt(PTT_File,ParsedPTT),
+	filter_genes(ParsedPTT,RawGenome,Filtered),
 	write('output is :'),
 	write(Filtered),
 	nl.
