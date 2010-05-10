8107257695991b920c55444a5fb795cca311c6b1
Matthieu Petit <petit@mox.ruc.dk>
2010-05-10 16:44:55 +0200

    Update lost genefinder script

--- scripts/lost_genefinder.pl
+++ scripts/lost_genefinder.pl
@@ -5,9 +5,9 @@
 :- lost_include_api(misc_utils).
 :- lost_include_api(interface).
 :- lost_include_api(stats).
-%:- ['../tmp/hard_to_find.pl'].
-%:- ['../tmp/orf_U00096_+1.pl'].
-%:- ['../data/gb_fragment.seq'].
+:- lost_include_api(autoAnnotations).
+:- ['../data/orf_U00096_+1.seq'].
+:- ['../data/gb_fragment.seq'].
 
 %----------
 % Learning:
@@ -17,30 +17,35 @@
 %      Step4: Generate the correct annotation
 %      Step5: Learn from PRISM
 %---------
-script_learning :-
+script_learning(Gene_Type) :-
+        consult_gene(Gene_Type),
         get_coding_ORF(Data_Coding,Starts_Position),
         get_non_coding_ORF(Data_nonCoding),
-        generation_annotation_coding(Data_Coding,Starts_Position,Annotation_Coding),
-        build_annotation_noncoding(Data_nonCoding,Annotation_Non_Coding),
+        generation_annotation_coding(Data_Coding,Starts_Position,Gene_Type,Annotation_Coding),
+        build_annotation_noncoding(Data_nonCoding,Gene_Type,Annotation_Non_Coding),
         map(build_learning_terms,Annotation_Coding,Terms_Coding),
         map(build_learning_terms,Annotation_Non_Coding,Terms_Non_Coding),
+        pick_a_given_number(400,Terms_Coding,Terms_Coding2),
         pick_a_given_number(400,Terms_Non_Coding,Terms_Non_Coding2),
-        append(Terms_Coding,Terms_Non_Coding2,Terms_Learning),
+        append(Terms_Coding2,Terms_Non_Coding2,Terms_Learning),
         prism('../models/lost_genefinder/lost_genefinder.psm'),
         learn(Terms_Learning),
+        atom_concat('../models/lost_genefinder/Parameters/',Gene_Type,Proba_File),
+        atom_concat(Proba_File,'.prb',Proba_File2),
         check_or_fail(
-		      save_sw('../models/lost_genefinder/parameters/test_hard.prb'),
+		      save_sw(Proba_File2),
 		      error(could_not_save_parameter_to_file(_ParametersFile))).
         
         
-
-
-
-
-
 %---
 % utils script learning
 %---
+
+consult_gene(Gene_Type) :-
+        atom_concat('../tmp/',Gene_Type,Gene_File),
+        atom_concat(Gene_File,'_to_find.pl',Gene_File2),
+        consult(Gene_File2).
+
 
 get_coding_ORF(Data_Coding,Starts_Position) :-
         findall([Data,(Min,Starts_Position)],(chunk(_,Min,Max,Data,Dir,_,_,_),hard_to_find(Starts_Position,Max,Dir,_)),Result),
@@ -66,12 +71,12 @@ divide_list([[A,B]|Rest],[[A|Rest_A],[B|Rest_B]]) :-
 
 
 
-generation_annotation_coding([],[],[]) :-
+generation_annotation_coding([],[],_Gene_Type,[]) :-
         !.
  
-generation_annotation_coding([Data|Data_Coding],[(Start_ORF,Start_Coding)|Starts_Position],[[Data,Annotation]|Rest_Annotation]) :-
+generation_annotation_coding([Data|Data_Coding],[(Start_ORF,Start_Coding)|Starts_Position],Gene_Type,[[Data,Gene_Type,Annotation]|Rest_Annotation]) :-
         generate_list_annotation(Data,Start_ORF,Start_Coding,Annotation),
-        generation_annotation_coding(Data_Coding,Starts_Position,Rest_Annotation).
+        generation_annotation_coding(Data_Coding,Starts_Position,Gene_Type,Rest_Annotation).
         
         
 generate_list_annotation([],_Start_ORF,_Start_Coding,[]) :-
@@ -89,12 +94,12 @@ generate_list_annotation([_N|Rest_Data],Start_ORF,Start_Coding,[0|Rest_Annot]) :
         generate_list_annotation(Rest_Data,Start_ORF1,Start_Coding,Rest_Annot).
 
 
-build_annotation_noncoding([],[]) :-
+build_annotation_noncoding([],_Gene_Type,[]) :-
         !.
 
-build_annotation_noncoding([Data|Data_nonCoding],[[Data,Annotation]|Annotation_Non_Coding]) :-
+build_annotation_noncoding([Data|Data_nonCoding],Gene_Type,[[Data,Gene_Type,Annotation]|Annotation_Non_Coding]) :-
         generate_list_noncoding(Data,Annotation),
-        build_annotation_noncoding(Data_nonCoding,Annotation_Non_Coding).
+        build_annotation_noncoding(Data_nonCoding,Gene_Type,Annotation_Non_Coding).
 
 
 
@@ -106,6 +111,9 @@ generate_list_noncoding([_V|Rest_Data],[0|Rest_Annotation]) :-
 
 
 pick_a_given_number(0,_,[]) :-
+        !.
+
+pick_a_given_number(Num,[],[]) :-
         !.
 
 pick_a_given_number(Num,[Elt|Rest],[Elt|Rest_Res]) :-
