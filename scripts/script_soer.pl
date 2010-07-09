7b474255a843dac3a0a085431139771d550a6595
Matthieu Petit <petit@ruc.dk>
2010-07-09 14:39:57 +0200

    Update of script_soer

--- scripts/script_soer.pl
+++ scripts/script_soer.pl
@@ -9,8 +9,8 @@
 %----
 
 
-% iid model
+% test model
 
-test_iid_model(OutputFile) :-
-        lost_sequence_file('DT_soer_models',InputFile),
-        run_model(soer_iid,annotate([InputFile],[],OutputFile)).  % default usage of model: data_available = yes, data_functor = sequence
+test_model(NameModel,Options,O) :-
+        lost_test_file('DT_soer_models',InputFile),
+        run_model(NameModel,annotate([InputFile],Options,OutputFile)). 
