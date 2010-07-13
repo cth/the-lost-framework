d16bff45643aafdf7a6ac516d7760f70b28d0199
Matthieu Petit <petit@ruc.dk>
2010-07-13 10:19:39 +0200

    learning ecoparse

--- models/ecoparse_adph/interface.pl
+++ models/ecoparse_adph/interface.pl
@@ -9,15 +9,20 @@
 
 % Input Format Specification
 lost_input_formats(annotate,[text(prolog(ranges(_)))]).
+lost_input_formats(learn,[text(prolog(ranges(_)))]).
 % Output Format Specification
 lost_output_format(annotate,_,text(prolog(ranges(_)))).
-
+lost_output_format(learn,_,).
 
 % Option declaration
 % Annotate options
 lost_option(annotate,use_parameter_file,yes,'Load parameters from the parameter file').
 lost_option(annotate,data_available,yes,'Specify if the sequence data is available or not in Extra info of the terms that composed the Input file').
 lost_option(annotate,data_functor,sequence,'Specify the name of the functor associated with the sequence data').
+% Learning options
+lost_option(learn,data_available,yes,'Specify if the sequence data is available or not in Extra info of the terms that composed the Input file').
+lost_option(learn,data_functor,sequence,'Specify the name of the functor associated with the sequence data').
+lost_option(learn,terms_number,undefined,'Define the number of terms used to learn the parameters of the models').
 
 
 % 
@@ -57,6 +62,28 @@ annotate([InputFile],Options,OutputFile) :-
         compute_and_save_annotations(Stream_Out,Data_Extraction).
 
 
+
+learn([InputFile,RawGenome],Options,OutputFile) :-
+        terms_from_file(InputFile,Terms),
+        get_option(Options,data_available,Data_Available),
+        get_option(Options,terms_number,Num),
+        (Data_Available == yes ->
+            % Caes: Data available in InputFile
+            get_option(Options,data_functor,Functor_Name)
+            get_data_from_terms(Terms,Functor,Num,List_Data) % Warning: same name that the one used in io but not the same arity
+        ;
+            get_ranges_from_terms(Terms,Num,Ranges),
+            get_data_from_file(RawGenome,[ranges(Ranges)],List_Data)
+        ),
+      % Learning process
+        map(build_learning_terms,List_Data,Learning_Terms),
+        check_or_fail(learn(Learning_Terms),
+                      error(learning_computation_ecoparse_adph)
+                     ),
+        save_sw(OutputFile),
+        move_data(OutputFile,'./Parameters/ecoparse_adph.prb'), % Hypothes: current directory is the model directory
+        save_sw_h('./Parameters/ecoparse_adph_h.prb').
+        
         
 %----
 % utils annotate
@@ -85,6 +112,77 @@ compute_and_save_annotations(Stream,[[Data,Key,Left,Right,Strand,Frame]|Rest_Dat
         write(Stream,'.'),
         nl(Stream),
         compute_and_save_annotations(Stream,Rest_Data).
-                      
-       
 
+
+%----
+% utils Learning
+%----
+
+
+
+
+% get_data_from_terms(Terms,Functor,Num,List_Data) 
+%
+% Warning: same name that the one used in io but the predicate has not the  same arity
+
+% Default value for Num = undefined that means all the value of data.
+get_data_from_terms(Terms,Functor,undefined,List_Data) :-
+        !,
+       Sequence =..[Functor,_],
+       findall(Data,(member(Term,Terms),
+                     Term =.. [_,_Key,_Left,_Right,_Strand,_Frame,Extra_Info],
+                     member(Sequence,Extra_Info),
+                     Sequence =.. [Functor,Data]
+                    ),
+                    List_Data
+              ).
+
+% Case: List Terms empty before Num = 0
+get_data_from_terms([],_Functor,Num,[]) :-
+        Num >= 0,
+        !.
+
+% Case: Num = 0, number of data to collect reached
+get_data_from_terms(_Terms,_Functor,0,[]) :-
+        !.
+
+
+get_data_from_terms([Term|Rest_Terms],Functor,Num,[Data|Rest_Data]) :-
+        Num > 0,
+        !,
+       Sequence =..[Functor,_],
+       Term =.. [_,_Key,_Left,_Right,_Strand,_Frame,Extra_Info],
+       member(Sequence,Extra_Info),
+       Sequence =.. [Functor,Data],
+       Num1 is Num-1,
+       get_data_from_terms(Rest_Terms,Functor,Num1,Rest_Data).
+
+
+
+% get_ranges_from_terms(Terms,Functor,Num,List_Data) 
+% Default value for Num = undefined that means all the ranges
+get_ranges_from_terms(Terms,undefined,List_Data) :-
+        !,
+       findall([Left,Right],Data,(member(Term,Terms),
+                     Term =.. [_,_Key,Left,Right,_Strand,_Frame,Extra_Info],
+                     Sequence =.. [Functor,Data]
+                    ),
+                    List_Data
+              ).
+
+% Case: List Terms empty before Num = 0
+get_ranges_from_terms([],Num,[]) :-
+        Num >= 0,
+        !.
+
+% Case: Num = 0, number of data to collect reached
+get_ranges_from_terms(_Terms,0,[]) :-
+        !.
+
+
+get_ranges_from_terms([Term|Rest_Terms],Num,[Range|Rest_Ranges]) :-
+        Num > 0,
+        !,
+       Term =.. [_,_Key,Left,Right,_Strand,_Frame,Extra_Info],
+       Num1 is Num-1,
+       get_ranges_from_terms(Rest_Terms,Functor,Num1,Rest_Data).
