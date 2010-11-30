140dd70ec0fd1ff844e0fbaa04fa83c24326dce3
Christian Theil Have <cth@mox.ruc.dk>
2010-11-30 20:15:11 +0100

    fix frame reported wrongly be genemark for reverse strand

--- models/genemark/ldata_parser.pl
+++ models/genemark/ldata_parser.pl
@@ -5,6 +5,7 @@
 :- lost_include_api(misc_utils).
 :- lost_include_api(utils_parser_report).
 
+
 ldata_parser_main(InputFile,OutputFile) :-
 	open(InputFile,read,InputStream2),
 	open(OutputFile,write,OStream),
@@ -53,21 +54,25 @@ ldata_line(T) --> frame_shift_prediction_line(T).
 
 % This is the format which seems to be used by the local version of
 % genemark
-gene_prediction_line(genemark_gene_prediction(Start,End,Strand,Frame,[average_probability(AvgProb),start_codon_probability(StartProb)])) -->
+gene_prediction_line(genemark_gene_prediction(na,Start,End,Strand,Frame,[average_probability(AvgProb),start_codon_probability(StartProb)])) -->
         [Start,End],
         reading_frame6(Strand,Frame),
         [AvgProb,StartProb],
 	{ integer(Start),integer(End), float(AvgProb),float(StartProb) }.
 
 % this is the format which seems to be used by the web genemark reports:
-gene_prediction_line(genemark_gene_prediction(Start,End,Strand,Frame,[average_probability(AvgProb),start_codon_probability(StartProb)])) -->
+gene_prediction_line(genemark_gene_prediction(na,Start,End,Strand,Frame,[average_probability(AvgProb),start_codon_probability(StartProb)])) -->
         [Start,End],
         strand_word(Strand),
         ['fr'],
-        [Frame],
+        [GenemarkFrame],
         [AvgProb,StartProb],
-	{ integer(Start),integer(End), integer(Frame),float(AvgProb),float(StartProb) }.
+	{ integer(Start),integer(End), fix_genemark_frame(GenemarkFrame,Strand,Frame), integer(Frame),float(AvgProb),float(StartProb) }.
 
+fix_genemark_frame(F,'+',F).
+fix_genemark_frame(1,'-',2).
+fix_genemark_frame(2,'-',3).
+fix_genemark_frame(3,'-',1).
 
 region_prediction_line(genemark_region_prediction(Start,End,Strand,Frame)) -->
         [Start,End],
