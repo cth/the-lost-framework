0c33fd16e721f9db2cccba1f186e5e983b156236
Christian Theil Have <christiantheilhave@gmail.com>
2011-04-04 22:57:34 +0200

    orf_length code update

--- models/orf_length/interface.pl
+++ models/orf_length/interface.pl
@@ -6,9 +6,39 @@
 
 :- [smooth].
 
+% OrfFile format:
 annotate([OrfFile,ParamFile],_Opts,OutputFile) :-
+	prism(length_model),
+	restore_sw(ParamFile),
+	open(OrfFile,read,InStream),
+	open(OutputFile,write,OutStream),
+	read_and_annotate_orf(InStream,OutStream),
+	close(InStream),
+	close(OutStream).
 	
-
+read_and_annotate_orf(InStream,OutStream) :-
+	write('.'),
+	read(InStream,ORF),
+	((ORF == end_of_file) ->
+		true
+	;
+		ORF =.. [Functor,SeqId,Left,Right,Dir,Frame,Extra],
+		member(starts(StartsList), Extra),
+		member(stop([Stop]), Extra),
+		findall(L,(member(Start,StartsList), length_in_codons(Start,Stop,L)), Lengths),!,
+		findall(R,(member(L,Lengths),get_range(L,R)),Ranges),!,
+		findall(P,(member(R,Ranges),prob(msw_length_range(R),P)),LengthScores),!,
+		NewExtra = [length_ranges(Ranges),length_scores(LengthScores)|Extra],
+		NewAnnot =.. [Functor,SeqId,Left,Right,Dir,Frame,NewExtra],
+%		write(NewAnnot),
+		write(OutStream,NewAnnot),
+		write(OutStream,'\n'),
+		!,
+		read_and_annotate_orf(InStream,OutStream)).
+		
+length_in_codons(Start,Stop,Codons) :-
+	Codons is abs(Start - Stop) // 3.
+	
 learn([GeneRefFile],_,OutputFile) :-
 	terms_from_file(GeneRefFile,GeneTerms),
 	extract_gene_lengths(GeneTerms,GeneLengths),
@@ -16,14 +46,19 @@ learn([GeneRefFile],_,OutputFile) :-
 	['length_model.psm'],
 	random_split_list(GeneLengths,RandomHalf,_),
 	learn_lengths(RandomHalf),
+	learn_lengths(GeneLengths),
 	save_sw(OutputFile),
 	!,
 	terms_from_file(OutputFile,[switch(length_range,_,Outcomes,Probs)]),
-	tell('/tmp/range_probs_20_vb_xval3.dat'),
+	tell('/tmp/range_probs_salmonella_250_vb.dat'),
 	write_data(Outcomes,Probs),
 	told.
 	
-write_date([],[]).
+
+
+%% Some utility predicates:
+	
+write_data([],[]).
 write_data([O|Os],[P|Ps]) :-
 	write(O),
 	write('\t'),
@@ -32,14 +67,13 @@ write_data([O|Os],[P|Ps]) :-
 	write_data(Os,Ps).
 
 extract_gene_lengths([],[]).
-
-
 extract_gene_lengths([G|Gs],[L|Ls]) :-
 	G =.. [_Func,_Id,Start,End,_Strand,_Frame,_extra],
 	NucleotideLength is abs(Start - End),
 	L is NucleotideLength // 3,
 	extract_gene_lengths(Gs,Ls).
 
+% For x-validation experiment
 random_split_list([],[],[]).	
 random_split_list([L|Ls],FirstHalf,SecondHalf) :-
 	random_uniform(1.0,X),
@@ -50,7 +84,3 @@ random_split_list([L|Ls],FirstHalf,SecondHalf) :-
 		FirstHalf = Fs,
 		SecondHalf = [L|Ss]),
 	random_split_list(Ls,Fs,Ss).
-
-
-%% Utility predicates:
-
--- scripts/orf_length_test.pl
+++ scripts/orf_length_test.pl
@@ -1,7 +1,19 @@
 :- ['../lost.pl'].
 
-test :-
-	lost_sequence_file('U00096_ptt',PTTFile),
-    run_model(parser_ptt,annotate([PTTFile],[],DatabaseFile)),
+%orfs(ecoli_all_orfs).
+orfs(test_orfs).
+
+test_learn(OutFile) :-
+	%lost_sequence_file('U00096_ptt',PTTFile),
+	lost_sequence_file('salmonella_ptt',PTTFile),
+        run_model(parser_ptt,annotate([PTTFile],[],DatabaseFile)),
 	run_model(orf_length,learn([DatabaseFile],[],OutFile)),
 	write('params written to outfile: '), write(OutFile),nl.
+
+test_annotate :-
+	test_learn(ParametersFile),
+	orfs(Orfs),
+	lost_sequence_file(Orfs,OrfsFile),
+	run_model(orf_length,annotate([OrfsFile,ParametersFile],[],AnnotationFile)),
+	write('Annotations written to file: '), write(AnnotationFile),nl.
+	
