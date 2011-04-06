3f9f39de886e3a7bdc79c5343a20156b22247752
Christian Theil Have <christiantheilhave@gmail.com>
2011-04-06 14:12:42 +0200

    hmm type length model

--- models/orf_length/interface.pl
+++ models/orf_length/interface.pl
@@ -6,8 +6,10 @@
 
 :- [smooth].
 
-% OrfFile format:
-annotate([OrfFile,ParamFile],_Opts,OutputFile) :-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Prediction using a HMM-type model for ORF chunks
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+annotate_orf_chunks([OrfFile,ParamFile],_Opts,OutputFile) :-
 	prism(length_model),
 	restore_sw(ParamFile),
 	open(OrfFile,read,InStream),
@@ -15,7 +17,7 @@ annotate([OrfFile,ParamFile],_Opts,OutputFile) :-
 	read_and_annotate_orf(InStream,OutStream),
 	close(InStream),
 	close(OutStream).
-	
+
 read_and_annotate_orf(InStream,OutStream) :-
 	write('.'),
 	read(InStream,ORF),
@@ -27,35 +29,104 @@ read_and_annotate_orf(InStream,OutStream) :-
 		member(stop([Stop]), Extra),
 		findall(L,(member(Start,StartsList), length_in_codons(Start,Stop,L)), Lengths),!,
 		findall(R,(member(L,Lengths),get_range(L,R)),Ranges),!,
+		viterbi(hmm_model(Lengths,Annotations)),
 		findall(P,(member(R,Ranges),prob(msw_length_range(R),P)),LengthScores),!,
 		NewExtra = [length_ranges(Ranges),length_scores(LengthScores)|Extra],
+		NewExtra = [start_annotations(Annotations),length_scores(LengthScores)|Extra],
 		NewAnnot =.. [Functor,SeqId,Left,Right,Dir,Frame,NewExtra],
 %		write(NewAnnot),
 		write(OutStream,NewAnnot),
-		write(OutStream,'\n'),
+		write(OutStream,'.\n'),
 		!,
 		read_and_annotate_orf(InStream,OutStream)).
+	
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Learning a HMM-type model from ORF chunks
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+learn_hmm_from_orfs([GeneRefFile,OrfFileRef],_,OutputFile) :-
+	terms_from_file(GeneRefFile,GeneRefTerms),
+	open(OrfFileRef,read,InStream),
+	lost_tmp_directory(TmpDir),
+	atom_concat(TmpDir,'/learn_length_hmm_from_orfs.pl',GoalsFile),
+	open(GoalsFile,write,OutStream),
+	build_hmm_goals(0,GeneRefTerms,InStream,OutStream),
+	close(OutStream),
+	close(InStream),
+	terms_from_file(GoalsFile,Goals),
+	learn(Goals),
+	save_sw(OutputFile).
+
+build_hmm_goals(Counter,GeneRefTerms,InStream,OutStream) :-
+	Counter1 is Counter + 1,
+	((0 is Counter1 mod 1000) -> write('procesed '), write(Counter1), write(' orfs'),nl ;	true),
+	read(InStream,OrfTerm),
+	((OrfTerm == end_of_file) ->
+		true
+	;
+		OrfTerm =.. [_Functor,_SeqId,_Left,_Right,Dir,Frame,Extra],
+		member(starts(StartsList), Extra),
+		(member(stop([Stop]), Extra) -> % Some chunks do not have stops
+			get_reference_starts(GeneRefTerms,Stop,Dir,Frame,CorrectStarts),
+			findall(L,(member(Start,StartsList), length_in_codons(Start,Stop,L)), Lengths),
+			lengths_annotation(StartsList,CorrectStarts,Annot1),
+			fill_internal('-',Annot1,Annot2),
+			write(OutStream,hmm_model(Lengths,Annot2)),
+				write(OutStream,'.\n')
+			;
+				true % Just skip those
+		),
+		!,
+		build_hmm_goals(Counter1,GeneRefTerms,InStream,OutStream)).
+
+% Rewrite annnotations like [-,-,+,-,-] to [-,-,+,+,+]
+fill_internal(_,[],[]).
+fill_internal('+',[_|As],['+'|Bs]) :-
+	fill_internal('+',As,Bs).
+fill_internal('-',[A|As],[A|Bs]) :-
+	fill_internal(A,As,Bs).
+
+lengths_annotation([],_CorrectStarts,[]).
+lengths_annotation([S|Ss],CorrectStarts,[A|As]) :-
+	(member(S,CorrectStarts) ->
+		A = '+'
+		;
+		A = '-'),
+		lengths_annotation(Ss,CorrectStarts,As).
 		
+get_reference_starts(GeneRefTerms,Stop,Dir,Frame,Starts) :-
+	findall(Start,stop_match(GeneRefTerms,Stop,Dir,Frame,Start),Starts).
+
+stop_match(GeneRefTerms,Stop,Dir,Frame,Start) :-
+	GeneRefTerms = [FirstTerm|_],
+	FirstTerm =.. [GeneRefFunctor|_],
+	((Dir=='+') ->
+		MatchStop is Stop + 2,
+		Matcher =.. [GeneRefFunctor,_,Start,MatchStop,Dir,Frame,_]
+		;
+		MatchStop is Stop - 2,
+		Matcher =.. [GeneRefFunctor,_,MatchStop,Start,Dir,Frame,_]),
+	member(Matcher,GeneRefTerms).
+
+
+%%%%%%%%%%%%%%%%%%% End learn goals %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/*
+learn_filter_probability(GeneTerms,PredictionTerms) :-
+	range_match_terms(GeneTerms,GeneMatchTerms),
+	range_match_terms(PredictionTerms,PredictionMatchTerms),
+	intersection(GeneMatchTerms,PredictionMatchTerms,Intersection),
+	length(PredictionTerms,NumPredictionTerms),
+	length(Intersection,NumMatching),
+	
+	set_sw()
+*/	
+
+
+
 length_in_codons(Start,Stop,Codons) :-
-	Codons is abs(Start - Stop) // 3.
+	Codons is abs(Start - Stop) // 3.	
 	
-learn([GeneRefFile],_,OutputFile) :-
-	terms_from_file(GeneRefFile,GeneTerms),
-	extract_gene_lengths(GeneTerms,GeneLengths),
-	prism(length_model),
-	['length_model.psm'],
-	random_split_list(GeneLengths,RandomHalf,_),
-	learn_lengths(RandomHalf),
-	learn_lengths(GeneLengths),
-	save_sw(OutputFile),
-	!,
-	terms_from_file(OutputFile,[switch(length_range,_,Outcomes,Probs)]),
-	tell('/tmp/range_probs_salmonella_250_vb.dat'),
-	write_data(Outcomes,Probs),
-	told.
-	
-
-
 %% Some utility predicates:
 	
 write_data([],[]).
@@ -84,3 +155,11 @@ random_split_list([L|Ls],FirstHalf,SecondHalf) :-
 		FirstHalf = Fs,
 		SecondHalf = [L|Ss]),
 	random_split_list(Ls,Fs,Ss).
+	
+%orf_to_range_facts(ORF,RangeFacts) :-
+%	ORF =.. [_Functor,SeqId,_Left,_Right,Dir,Frame,Extra],
+%	member(starts(Starts), Extra),
+%	member(stop([Stop]), Extra),
+%	findall(RangeFact,(	member(Start,Starts),
+%						RangeFact=range_fact(SeqId,Start,Stop,Dir,Frame,[])),
+%			RangeFacts).
