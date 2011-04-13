59650e04d0f96a81da025f6e57176272f96d615c
Christian Theil Have <christiantheilhave@gmail.com>
2011-04-13 18:08:50 +0200

    new parse goal, which avoids dependency on Genbank file (which seems silly if you are parsing a fasta file from a different place...)

--- models/parser_fna/interface.pl
+++ models/parser_fna/interface.pl
@@ -7,13 +7,27 @@
 % Option declaration
 lost_option(annotate,list,'none','Divided sequences of Nucleotids into pieces').
 lost_option(annotate,range,[min,max],'Define a range to extract a sub-part of the genome').
+lost_option(parse,list,'none','Divided sequences of Nucleotids into pieces').
+lost_option(parse,range,[min,max],'Define a range to extract a sub-part of the genome').
+
+%lost_option(annotate,sequenceid,[min,max],'Define a range to extract a sub-part of the genome').
 
 % Input Format Specification
 lost_input_formats(annotate,[text(fna),text(gbk)]).
+lost_input_formats(parse,[text(fna)]).
 % Output Format Specification
 lost_output_format(annotate,_,text(prolog(sequence(dna)))).
+lost_output_format(parse,_,text(prolog(sequence(dna)))).
 
 
 annotate([FNA_File,GBK_File],Options,OutputFile) :-
         consult(fna_parser),
-        parser_fna(Options,FNA_File,GBK_File,OutputFile).
+        open(GBK_File,read,GBK_Stream),
+        get_info_gbk(GBK_Stream,GeneBank_Key,N_BP),
+        close(GBK_Stream),
+        parser_fna(Options,FNA_File,GeneBank_Key,N_BP,OutputFile).
+
+parse([FNA_File],Options,OutputFile) :-
+        maxint(Max),
+        consult(fna_parser),
+        parser_fna(Options,FNA_File,'n/a',Max,OutputFile).
