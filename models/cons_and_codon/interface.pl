18f91837ca230a1bfce7742cf3c3881eb9635784
Christian Theil Have <christiantheilhave@gmail.com>
2011-05-05 14:10:47 +0200

    quick fix for wrong start annotations

--- models/cons_and_codon/interface.pl
+++ models/cons_and_codon/interface.pl
@@ -85,65 +85,73 @@ make_training_file_rec_joint_model(InStream,OutStream) :-
 		make_training_file_rec_joint_model(InStream,OutStream)).
 
 
-%%%%%%%%%%%%%%%%%%%%%
-% Basic combiner    %
-%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% codpref (with gene grammar    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-annotate_combiner([ParamsFile,InputFile],_Options,OutputFile) :-                                 
-   	prismAnnot('combiner'), % Load the actual PRISM model
+annotate_codpref([ParamsFile,InputFile],_Options,OutputFile) :-
+   	prismAnnot('codpref'), % Load the actual PRISM model
         restore_sw(ParamsFile),
 	open(InputFile,read,StreamIn),
 	open(OutputFile,write,StreamOut),
-	compute_and_save_annotations_combiner(StreamIn,StreamOut,1),
+	compute_and_save_annotations_codpref(StreamIn,StreamOut,1),
 	close(StreamOut),
 	close(StreamIn).
 
-parallel_annotate_combiner([ParamsFile,InputFile],_,OutputFile) :-
+parallel_annotate_codpref([ParamsFile,InputFile],_,OutputFile) :-
         split_file(InputFile,500,'blastgf_single_input', '.pl', ResultingFiles),
         open('input_files.list',write,OutS),
         forall(member(File,ResultingFiles),(write(OutS,File),write(OutS,'\n'))),
         close(OutS),
-        atom_concat_list(['./parallel_predict.sh ','8 ', 'annotate_combiner ', ParamsFile, ' ', OutputFile, ' input_files.list'], Cmd),
+        atom_concat_list(['./parallel_predict.sh ','12 ', 'annotate_codpref ', ParamsFile, ' ', OutputFile, ' input_files.list'], Cmd),
         system(Cmd).
 
-parallel_learn_combiner([ChunkFileWithRef],_,ParamsFile) :-
+parallel_learn_codpref([ChunkFileWithRef],_,ParamsFile) :-
         lost_tmp_directory(Tmp),
-        atom_concat_list([Tmp,'combiner_full_train.pl'],TrainingFile),
-        make_training_file_combiner(ChunkFileWithRef,TrainingFile),
-        split_file(TrainingFile,1000,'input_train_combiner', '.pl',ResultingFiles),
+        atom_concat_list([Tmp,'codpref_full_train.pl'],TrainingFile),
+        make_training_file_codpref(ChunkFileWithRef,TrainingFile),
+        split_file(TrainingFile,1000,'input_train_codpref', '.pl',ResultingFiles),
         open('training_files.list',write,OutS),
         forall(member(File,ResultingFiles), (write(OutS,File), write(OutS,'\n'))),
         close(OutS),
-        atom_concat_list(['./parallel_train.sh ','6 ', 'combiner ',  ParamsFile, ' training_files.list'], Cmd),
+        atom_concat_list(['./parallel_train.sh ','12 ', 'codpref ',  ParamsFile, ' training_files.list'], Cmd),
         system(Cmd).
 
 
-learn_combiner([ChunkFileWithRef],_,ParamsFile) :-
+learn_codpref([ChunkFileWithRef],_,ParamsFile) :-
 	TrainingFile = 'training_data.pl',
-	make_training_file_combiner(ChunkFileWithRef,TrainingFile),
-        prism(combiner),
-        ['combiner.psm'],
+	make_training_file_codpref(ChunkFileWithRef,TrainingFile),
+        prism(codpref),
+        ['codpref.psm'],
 	viterbi_learn_file(TrainingFile),
 	save_sw(ParamsFile).
 
 
-compute_and_save_annotations_combiner(StreamIn,Stream_Out,Nb_Iterations) :-
+compute_and_save_annotations_codpref(StreamIn,Stream_Out,Nb_Iterations) :-
 		read(StreamIn,Chunk),
 		((Chunk == end_of_file) ->
 			true,
 			write('compute_and_save_annotations done.'),nl
 			;
 			Chunk =.. [ _F,SeqId, Left, Right, Strand, Frame, Extra],
-                        member(blastgf(Prediction),Extra),
                         member(sequence(Nucleotides),Extra),
-                merge_nucleotides_and_prediction(Nucleotides,Prediction,Combined),
+                nucleotide_triplets(Nucleotides,Triplets),
 
-        	check_or_fail(viterbit(combiner_noannot(Combined),_P,Tree),error('Viterbi computation failed')),
+                write(viterbit(combiner_noannot(Triplets),P,Tree)),nl,
+
+                % We use soft check instead, since some ORFs may not
+                % have stops and this will fail for those
+        	%check_or_fail(viterbit(combiner_noannot(Combined),_P,Tree),error('Viterbi computation faile (combiner length')),
+        	(viterbit(combiner_noannot(Triplets),_P,Tree) ->
+                        true
+                        ;
+                        write('Viterbi computation failed for goal: '),
+                        write(combiner_noannot(Triplets))),
                 flatten(Tree,FlatTree),
                 findall(State,member(msw(emit(State),_),FlatTree),States),
                 reverse(States,RevStates),
                 build_state_annotation(RevStates,Annotation),
-        	build_term_for_annotation(combiner,SeqId,[Left,Right],Strand,Frame,Annotation,Term), (var(Term) ->
+        	build_term_for_annotation(codpref,SeqId,[Left,Right],Strand,Frame,Annotation,Term), (var(Term) ->
             	true
         		;
             	write(Stream_Out,Term),write(Stream_Out,'.'),nl(Stream_Out)
@@ -158,30 +166,29 @@ compute_and_save_annotations_combiner(StreamIn,Stream_Out,Nb_Iterations) :-
         	),
 			Nb_Iterations1 is Nb_Iterations+1,
         	!,
-        	compute_and_save_annotations_combiner(StreamIn,Stream_Out,Nb_Iterations1)).
+        	compute_and_save_annotations_codpref(StreamIn,Stream_Out,Nb_Iterations1)).
 
-make_training_file_combiner(Chunk_Annot,Training_File):-
+make_training_file_codpref(Chunk_Annot,Training_File):-
 	open(Chunk_Annot,read,Chunk_Stream),
 	open(Training_File,write,Train_Stream),
-	make_training_file_combiner_rec(Chunk_Stream, Train_Stream),
+	make_training_file_codpref_rec(Chunk_Stream, Train_Stream),
 	close(Train_Stream),
 	close(Chunk_Stream).
 	
-make_training_file_combiner_rec(InStream,OutStream) :-
+make_training_file_codpref_rec(InStream,OutStream) :-
 	read(InStream,Chunk),
 	((Chunk==end_of_file) ->
 		true 
 		;
 		Chunk =.. [ _Functor, _SeqId, _Left, _Right, _Strand, _Frame, Extra],
 		member(gb(RefAnnot),Extra),
-                member(blastgf(Prediction),Extra),
                 member(sequence(Nucleotides),Extra),
-                merge_nucleotides_and_prediction(Nucleotides,Prediction,Combined),
+                nucleotide_triplets(Nucleotides,Triplets),
 %                write(merge_nucleotides_and_prediction(Nucleotides,Prediction,Combined)),
-                Goal =.. [ combiner, Combined,  RefAnnot ], 
+                Goal =.. [ combiner, Triplets, RefAnnot ], 
 		write(OutStream,Goal), 
 		write(OutStream,'.\n'),
-		make_training_file_combiner_rec(InStream,OutStream)).
+		make_training_file_codpref_rec(InStream,OutStream)).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -326,27 +333,19 @@ annotate_codpref_w_length([ParamsFile,InputFile],_Options,OutputFile) :-
         restore_sw(ParamsFile),
 	open(InputFile,read,StreamIn),
 	open(OutputFile,write,StreamOut),
-	compute_and_save_annotations_combiner_length(StreamIn,StreamOut,1),
+	compute_and_save_annotations_codpref_w_length(StreamIn,StreamOut,1),
 	close(StreamOut),
 	close(StreamIn).
 
 
 parallel_annotate_codpref_w_length([ParamsFile,InputFile],_,OutputFile) :-
-        split_file(InputFile,500,'cTripletsodpref_w_length_input', '.pl', ResultingFiles),
+        split_file(InputFile,500,'input', '.pl', ResultingFiles),
         open('input_files.list',write,OutS),
         forall(member(File,ResultingFiles),(write(OutS,File),write(OutS,'\n'))),
         close(OutS),
-        atom_concat_list(['./parallel_predict.sh ','10 ', 'codpref_w_length', ParamsFile, ' ', OutputFile, ' input_files.list'], Cmd),
+        atom_concat_list(['./parallel_predict.sh ','10 ', 'annotate_codpref_w_length ', ParamsFile, ' ', OutputFile, ' input_files.list'], Cmd),
+        nl,write(Cmd),nl,
         system(Cmd).
-
-parallel_annotate_codpref_w_length([ParamsFile,InputFile],_,OutputFile) :-
-        split_file(InputFile,500,'codpref_w_length_input', '.pl', ResultingFiles),
-        open('input_files.list',write,OutS),
-        forall(member(File,ResultingFiles),(write(OutS,File),write(OutS,'\n'))),
-        close(OutS),
-        atom_concat_list(['./parallel_predict.sh ','10 ', 'codpref_w_length', ParamsFile, ' ', OutputFile, ' input_files.list'], Cmd),
-        system(Cmd).
-
 
 compute_and_save_annotations_codpref_w_length(StreamIn,Stream_Out,Nb_Iterations) :-
 		read(StreamIn,Chunk),
@@ -358,9 +357,7 @@ compute_and_save_annotations_codpref_w_length(StreamIn,Stream_Out,Nb_Iterations)
                         member(sequence(Nucleotides),Extra),
                         member(length_range_annot(LengthRangeAnnot),Extra),
                 nucleotide_triplets(Nucleotides,Triplets),
-                write('length range annot: '), write(LengthRangeAnnot),nl,
-                merge_length_annot(Triplets,LengthRangeAnnot,Combined),
-
+                merge_length_annot(Triplets,LengthRangeAnnot, Combined),
                 write(viterbit(combiner_noannot(Combined),P,Tree)),nl,
 
                 % We use soft check instead, since some ORFs may not
@@ -404,7 +401,7 @@ compute_and_save_annotations_codpref_w_length(StreamIn,Stream_Out,Nb_Iterations)
 parallel_learn_codpref_w_length([ChunkFileWithRef],_,ParamsFile) :-
         lost_tmp_directory(Tmp),
         atom_concat_list([Tmp,'codpref_w_length_full_train.pl'],TrainingFile),
-        make_training_file_combiner_length(ChunkFileWithRef,TrainingFile),
+        make_training_file_codpref_w_length(ChunkFileWithRef,TrainingFile),
         split_file(TrainingFile,1000,'input_codpref_w_length', '.pl',ResultingFiles),
         open('training_files.list',write,OutS),
         forall(member(File,ResultingFiles), (write(OutS,File), write(OutS,'\n'))),
@@ -416,7 +413,7 @@ parallel_learn_codpref_w_length([ChunkFileWithRef],_,ParamsFile) :-
 learn_combiner_codpref_w_length([ChunkFileWithRef],_,ParamsFile) :-
 	TrainingFile = 'training_data.pl',
 	make_training_file_combiner(ChunkFileWithRef,TrainingFile),
-        prism(combiner_length),
+        prism(codpref_w_length),
         ['codpref_w_length.psm'],
 	viterbi_learn_file(TrainingFile),
 	save_sw(ParamsFile).
@@ -437,9 +434,8 @@ make_training_file_codpref_w_length_rec(InStream,OutStream) :-
 		Chunk =.. [ _Functor, _SeqId, _Left, _Right, _Strand, _Frame, Extra],
 		member(gb(RefAnnot),Extra),
                 member(sequence(Nucleotides),Extra),
-                member(length_range_annot(LengthRangeAnnot), Extra),
-                nucleotide_triplets(Nucleotides,Triplets),
-                merge_length_annot(Triplets,LengthRangeAnnot,Combined),
+                member(blastgf(Prediction),Extra),
+                merge_nucleotides_and_prediction(Nucleotides,Prediction,Combined),
                 Goal =.. [ combiner, Combined,  RefAnnot ], 
 		write(OutStream,Goal), 
 		write(OutStream,'.\n'),
@@ -447,6 +443,123 @@ make_training_file_codpref_w_length_rec(InStream,OutStream) :-
 
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Combiner: codpref and length 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+annotate_codpref_w_blast([ParamsFile,InputFile],_Options,OutputFile) :-
+   	prismAnnot('codpref_w_blast'), % Load the actual PRISM model
+        restore_sw(ParamsFile),
+	open(InputFile,read,StreamIn),
+	open(OutputFile,write,StreamOut),
+	compute_and_save_annotations_codpref_w_blast(StreamIn,StreamOut,1),
+	close(StreamOut),
+	close(StreamIn).
+
+
+parallel_annotate_codpref_w_blast([ParamsFile,InputFile],_,OutputFile) :-
+        split_file(InputFile,500,'input', '.pl', ResultingFiles),
+        open('input_files.list',write,OutS),
+        forall(member(File,ResultingFiles),(write(OutS,File),write(OutS,'\n'))),
+        close(OutS),
+        atom_concat_list(['./parallel_predict.sh ','10 ', 'annotate_codpref_w_blast ', ParamsFile, ' ', OutputFile, ' input_files.list'], Cmd),
+        nl,write(Cmd),nl,
+        system(Cmd).
+
+compute_and_save_annotations_codpref_w_blast(StreamIn,Stream_Out,Nb_Iterations) :-
+		read(StreamIn,Chunk),
+		((Chunk == end_of_file) ->
+			true,
+			write('compute_and_save_annotations_codpref_w_blast done.'),nl
+			;
+			Chunk =.. [ _F,SeqId, Left, Right, Strand, Frame, Extra],
+                        member(sequence(Nucleotides),Extra),
+                        member(blastgf(Prediction),Extra),
+
+                merge_nucleotides_and_prediction(Nucleotides,Prediction,Combined),
+
+                write(viterbit(combiner_noannot(Combined),P,Tree)),nl,
+
+                % We use soft check instead, since some ORFs may not
+                % have stops and this will fail for those
+        	%check_or_fail(viterbit(combiner_noannot(Combined),_P,Tree),error('Viterbi computation faile (combiner length')),
+        	(viterbit(combiner_noannot(Combined),_P,Tree) ->
+                        true
+                        ;
+                        write('Viterbi computation failed for goal: '),
+                        write(combiner_noannot(Combined))),
+                flatten(Tree,FlatTree),
+                findall(State,member(msw(emit(State),_),FlatTree),States),
+                reverse(States,RevStates),
+                write(RevStates),nl,
+                build_state_annotation(RevStates,Annotation),
+                write(Annotation),nl,
+        	write(build_term_for_annotation(codpref_w_blast,SeqId,[Left,Right],Strand,Frame,Annotation,Term)),nl, 
+        	build_term_for_annotation(codpref_w_blast,SeqId,[Left,Right],Strand,Frame,Annotation,Term), 
+                write('term is :'),
+                write(Term),nl,
+                (var(Term) ->
+            	        true
+        		;
+            	        write(Stream_Out,Term),write(Stream_Out,'.'),nl(Stream_Out)
+        	),
+        	Number is Nb_Iterations mod 500,
+        	(Number == 1 -> write(Nb_Iterations) ; write('.')),
+        	(Number == 0 ->
+            	table_remove(blastgf_annot(_,_)),
+            	table_remove(blastgf_rec_annot(_,_,_))
+        		;
+            	true
+        	),
+			Nb_Iterations1 is Nb_Iterations+1,
+        	!,
+        	compute_and_save_annotations_codpref_w_blast(StreamIn,Stream_Out,Nb_Iterations1)).
+
+
+
+
+parallel_learn_codpref_w_blast([ChunkFileWithRef],_,ParamsFile) :-
+        lost_tmp_directory(Tmp),
+        atom_concat_list([Tmp,'codpref_w_blast_full_train.pl'],TrainingFile),
+        make_training_file_codpref_w_blast(ChunkFileWithRef,TrainingFile),
+        split_file(TrainingFile,1000,'input_codpref_w_blast', '.pl',ResultingFiles),
+        open('training_files.list',write,OutS),
+        forall(member(File,ResultingFiles), (write(OutS,File), write(OutS,'\n'))),
+        close(OutS),
+        atom_concat_list(['./parallel_train.sh ','10 ', 'codpref_w_blast ',  ParamsFile, ' training_files.list'], Cmd),
+        system(Cmd).
+
+
+learn_combiner_codpref_w_blast([ChunkFileWithRef],_,ParamsFile) :-
+	TrainingFile = 'training_data.pl',
+	make_training_file_cod_pref_w_blast(ChunkFileWithRef,TrainingFile),
+        prism(codpref_w_blast),
+        ['codpref_w_blast.psm'],
+	viterbi_learn_file(TrainingFile),
+	save_sw(ParamsFile).
+
+
+make_training_file_codpref_w_blast(Chunk_Annot,Training_File):-
+	open(Chunk_Annot,read,Chunk_Stream),
+	open(Training_File,write,Train_Stream),
+	make_training_file_codpref_w_blast_rec(Chunk_Stream, Train_Stream),
+	close(Train_Stream),
+	close(Chunk_Stream).
+	
+make_training_file_codpref_w_blast_rec(InStream,OutStream) :-
+	read(InStream,Chunk),
+	((Chunk==end_of_file) ->
+		true 
+		;
+		Chunk =.. [ _Functor, _SeqId, _Left, _Right, _Strand, _Frame, Extra],
+		member(gb(RefAnnot),Extra),
+                member(sequence(Nucleotides),Extra),
+                member(blastgf(Prediction),Extra),
+                merge_nucleotides_and_prediction(Nucleotides,Prediction,Combined),
+                Goal =.. [ combiner, Combined,  RefAnnot ], 
+		write(OutStream,Goal), 
+		write(OutStream,'.\n'),
+		make_training_file_codpref_w_blast_rec(InStream,OutStream)).
 
 %%%%%%%%%%%% Utility predicates %%%%%%%%%%%%%%
 
@@ -460,15 +573,35 @@ build_state_annotation([stop|StatesRest],[1,1,1|AnnotRest]) :-
 build_state_annotation([c|StatesRest],[1,1,1|AnnotRest]) :-
         build_state_annotation(StatesRest,AnnotRest).
 
+build_term_for_annotation(Functor,SeqId,[Left,Right],Dir,Frame,Annotation,Term) :-
+        build_term_for_annotation_old(Functor,SeqId,[Left,Right],Dir,Frame,Annotation,TermOld),
+        quick_fix(TermOld,Term).
+
+quick_fix(Prediction,FixedPrediction) :-
+        Prediction =.. [Functor,SeqId,Left,Right,Dir,Frame,Extra],
+        member(annotation(Annot),Extra),
+        sumlist(Annot,CodingLength),
+        length(Annot,AnnotationLength),
+        (Dir=='+' ->
+                NewLeft is Left + (AnnotationLength - CodingLength),
+                NewRight is Right
+                ;
+                NewLeft is Left,
+                NewRight is Right - (AnnotationLength - CodingLength)
+        ),
+        FixedPrediction =.. [Functor,SeqId,NewLeft,NewRight,Dir,Frame,Extra].
+
+
+
 
 % build_term_for_annotation :
 % Terms is a variable if the annotation is a list of 0, otherwise a term with a range for the coding region
-build_term_for_annotation(Functor,SeqId,[Left,Right],Dir,Frame,Annotation,Term) :-
+build_term_for_annotation_old(Functor,SeqId,[Left,Right],Dir,Frame,Annotation,Term) :-
         first_coding(Left,1,Annotation,Start),
         !,
         Term =..[Functor,SeqId,Left,Right,Dir,Frame,[annotation(Annotation),start(Start)]].
 
-build_term_for_annotation(_Functor,_SeqId,_Range,_Dir,_Frame,_Annotation,_Term).
+build_term_for_annotation_old(_Functor,_SeqId,_Range,_Dir,_Frame,_Annotation,_Term).
 
 % first_coding(++Left,++Value,++Annotation,--Start)
 first_coding(_Left,_Value,[],_Start) :-
@@ -483,7 +616,7 @@ first_coding(Left,Value,[_Annotation|Rest_Annotations],Start) :-
         first_coding(Left1,Value,Rest_Annotations,Start).
 
 nucleotide_triplets([],[]).
-nucleotide_triplets([N1,N2,N3|Ns],[[N1,N2,N3]|Rest]) :-
+nucleotide_triplets([N1,N2,N3|Ns],[[[N1,N2,N3]]|Rest]) :-
         nucleotide_triplets(Ns,Rest).
 
 merge_nucleotides_and_prediction([],[],[]).
