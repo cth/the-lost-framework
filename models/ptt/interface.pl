28964dfce37378dc1f86b9ab45d13287a702728d
Christian Theil Have <christiantheilhave@gmail.com>
2011-07-15 11:26:42 +0200

    ptt parser

--- models/ptt/interface.pl
+++ models/ptt/interface.pl
@@ -4,14 +4,14 @@
 
 % Generate a file of predicate based on Easygene report.
 
-lost_input_formats(annotate, [text(ptt)]).
-lost_output_format(annotate, _Options, text(prolog(ranges(gene)))).
+lost_input_formats(parse, [text(ptt)]).
+lost_output_format(parse, _Options, text(prolog(ranges(gene)))).
 
 
 % Option
-lost_option(annotate,genome_key,'U00096', 'Specify the genome key of GeneBank.'). % Note: information not available in the PTT file
+lost_option(parse,genome_key,'U00096', 'Specify the genome key of GeneBank.'). % Note: information not available in the PTT file
 
-annotate([InputFile],Options,OutputFile) :-
+parse([InputFile],Options,OutputFile) :-
         consult(ptt_parser),
         get_option(Options,genome_key,Key),
         gb_parser(InputFile,Key,OutputFile).
