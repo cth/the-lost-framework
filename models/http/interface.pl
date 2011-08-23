3712b8f74d6a19f86a17cd29a4e6b5cc3e6288d9
Christian Theil Have <christiantheilhave@gmail.com>
2011-08-23 09:48:29 +0200

    http stuff

--- models/http/interface.pl
+++ models/http/interface.pl
@@ -1,13 +1,17 @@
 :- ['../../lost.pl'].
 :- lost_include_api(misc_utils).
 
+
+lost_input_formats(get, [url]).
+lost_output_format(get, _, file(unknown)).
+
 get([URL],_Options,OutputFile) :-
 	build_command(URL,OutputFile,Cmd),
 	writeln(Cmd),
 	system(Cmd).
 	
 build_command(URL,OutputFile,Cmd) :-
-	atom_concat_list([wget, ' ', URL, ' --output-document=', OutputFile],Cmd).
+	atom_concat_list([wget, ' "', URL, '" --output-document=', OutputFile],Cmd).
 	
 test :-
 	get(['ftp://ftp.ncbi.nih.gov/genomes/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.Prodigal-2.50'],[],'test.out').
