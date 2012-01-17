c4f930bb964b7c06af8919b7343d2b6814707653
Christian Theil Have <christiantheilhave@gmail.com>
2012-01-17 13:31:32 +0100

    print file names

--- scripts/experiments/pyl/infernal/trna_search.pl
+++ scripts/experiments/pyl/infernal/trna_search.pl
@@ -96,7 +96,7 @@ trna_pyl_hits(Org) <- organism(Org) | infernal::search([calibrated_model,genome(
 
 % test
 runall :- 
-	findall(Organism,organism(Organism),Organisms),
-	forall(member(Organism,Organisms), (run(trna_pyl_hits(Organism)) ; writeln('skipping ahead'))).
+	findall([Organism,File],(organism(Organism),run(trna_pyl_hits(Organism),get_result_file(trna_pyl_hits(Organism),File))),OrganismFiles),
+	forall(member([O,F],OrganismFiles),(write(O),write(': '), writeln(File))).
 
 
