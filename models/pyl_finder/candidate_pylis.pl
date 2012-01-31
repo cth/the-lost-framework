860f1dacf176de8d1d0919f13527e4a098547def
Christian Theil Have <christiantheilhave@gmail.com>
2012-01-31 15:31:16 +0100

    filtering of candidate pylis elements

---
+++ models/pyl_finder/candidate_pylis.pl
@@ -0,0 +1,58 @@
+:- use(genedb).
+
+pylis_size(100).
+
+extract_candidate_pylis(OrfsFile,OutputFile) :-
+	open(OrfsFile,read,InStream),
+	open(OutputFile,write,OutStream),
+	process_stream(InStream,OutStream),
+	close(InStream),
+	close(OutStream).	
+	
+	
+process_stream(InStream,OutStream) :-
+	read(InStream,OrfTerm),
+	((OrfTerm == end_of_file) ->
+		true
+		;
+		process_orf_term(OrfTerm,OutStream),
+		process_stream(InStream,OutStream)).
+	
+% Skip ORFs with more than one in frame amber codon
+process_orf_term(Orf,_) :-
+	gene_extra_field(Orf,in_frame_stops,Stops),
+	Stops > 1,
+	!.
+	
+process_orf_term(Orf,_OutStream) :-
+	gene_extra_field(Orf,in_frame_stops,[InFrameStop]),
+	gene_stop_codon(Orf,Stop),
+	distance(InFrameStop,Stop,Distance),
+	pylis_size(PylisSize),
+	Distance < PylisSize.
+	
+process_orf_term(Orf,OutStream) :-
+	gene_extra_field(Orf,in_frame_stops,[InFrameStop]),
+	gene_extra_field(Orf,sequence,Sequence),
+	gene_start_codon(Orf,Start),
+	distance(Start,InFrameStop,GeneBeforeUag),
+	prefix(GeneBeforeUag,Sequence,_Prefix,Suffix),
+	pylis_size(PylisSize),
+	prefix(PylisSize,Suffix,PylisSeq,_Rest),
+	gene_add_extra_field(Orf,pylis,PylisSeq,UpdatedOrf),
+	writeq(OutStream,UpdatedOrf),
+	writeln('.').
+	
+prefix(0,S,[],S).
+
+prefix(N,[S|Ss],[S|Ps],Suffix) :-
+	N1 is N - 1,
+	prefix(N1,Ss,Ps,Suffix).
+		
+distance(Pos1,Pos2,Dist) :-
+	Pos1 < Pos2,
+	distance(Pos2,Pos1,Dist).
+	
+distance(Pos1,Pos2,Dist) :-
+	Pos1 >= Pos2,
+	Dist is (Pos1 - Pos2) + 1.
--- models/pyl_finder/interface.pl
+++ models/pyl_finder/interface.pl
@@ -1,5 +1,7 @@
 :- task(candidate_orfs([text(fasta)],[],text(prolog(ranges(gene))))).
 
+:- task(candidate_pylis([text(prolog(ranges(gene)))],[extract_size(100)], text(prolog(ranges(gene))))).
+
 %% candidate_orfs(+InputFiles,+Options,+OutputFile)
 % ==
 % InputFiles = [ GenomeFastaFile ]
@@ -9,3 +11,15 @@
 candidate_orfs([GenomeFastaFile],Options,OutputFile) :-
 	cl(extract_orfs),
 	extract_orfs(GenomeFastaFile,OutputFile).
+
+
+%% candidate_pylis(+InputFiles,+Options,+OutputFile)
+% ==
+% InputFiles = [ OrfsFile ]
+% ==
+% Given a file with candidate orfs (with in frame amber codons), for each amber codon,
+% extract the a portion of the sequence downstream of the codon that may contain a PYLIS element. 
+% The exact size of the downstream sequence element is configurable.
+condidate_pylis([OrfsFile],Options,OutputFile) :-
+	cl(candidate_pylis),
+	extract_candidate_pylis(OrfsFile,OutputFile).
