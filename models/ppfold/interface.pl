d7808f1e080e6ab20deca8333178d62b70ab1a59
Christian Theil Have <christiantheilhave@gmail.com>
2012-02-01 10:42:32 +0100

    documentation for model

--- models/ppfold/interface.pl
+++ models/ppfold/interface.pl
@@ -4,6 +4,13 @@
 
 :- task(ppfold([text(prolog(ranges(gene)))], [sequence_functor(sequence)], text(prolog(ranges(gene))))).
 
+%% ppfold(+InputFiles,+Options,+OutputFile)
+% ==
+% InputFile = [InputFile]
+% == 
+% Folds a sequence annotation given as an extra of each gene and adds this as a new extra field
+% of the gene in the output file. The extra containing the sequence to fold is specified using the
+% =|sequence_functor|= option.
 ppfold(InputFile, Options, OutputFile) :-
 	get_option(Options,sequence_functor,Functor),
 	terms_from_file(InputFile,GeneTerms),
