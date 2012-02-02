b9fef0ba21486bcd7d30303446517cd5832faf32
Christian Theil Have <christiantheilhave@gmail.com>
2012-02-02 14:17:19 +0100

    a test task with a multi-file goal

--- models/test/interface.pl
+++ models/test/interface.pl
@@ -1,5 +1,13 @@
 :- task(test1([text,text],[opt1(a),opt2(b)],text)).
 :- task(has_no_implementation([text,text],[opt1(a),opt2(b)],text)).
 
+:- task(multifile([text],[],[text,text])).
+
 test1([I1,I2],Opts,Out) :-
 	writeln('dummy implementation of test1').
+	
+
+% A task with multiple output files
+multifile([InputFile],Options,[OutputFile1,OutputFile2]) :-
+	copy_file(InputFile,OutputFile1),
+	copy_file(InputFile,OutputFile2).
