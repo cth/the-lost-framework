4ec12f03c81e81d0367b182436dbda2c86c43f60
Christian Theil Have <christiantheilhave@gmail.com>
2012-02-10 14:40:40 +0100

    revert candidate_pylis.pl to old version

--- models/pyl_finder/candidate_pylis.pl
+++ models/pyl_finder/candidate_pylis.pl
@@ -1,24 +1,26 @@
 :- use(genedb).
 
-% extract_candidate_pyles(+OrfsFile,+OutputFile)
+pylis_size(100).
+
+%% extract_candidate_pyles(+OrfsFile,+OutputFile)
 % Reads each term from OrfsFile in a Stream
-extract_candidate_pylis(OrfsFile,OutputFile,MaxBasesDownstream) :-
+extract_candidate_pylis(OrfsFile,OutputFile) :-
 	open(OrfsFile,read,InStream),
 	open(OutputFile,write,OutStream),
-	process_stream(0,InStream,OutStream,MaxBasesDownstream),
+	process_stream(0,InStream,OutStream),
 	close(InStream),
-	close(OutStream).
+	close(OutStream).	
 	
 	
-process_stream(N,InStream,OutStream,MaxBasesDownstream) :-
+process_stream(N,InStream,OutStream) :-
 	read(InStream,OrfTerm),
 	((OrfTerm == end_of_file) ->
 		true
 		;
 		((0 is N mod 1000) -> write(N) ; ((0 is N mod 100) -> write('.') ; true)),
 		N1 is N + 1,
-		process_orf_term(OrfTerm,OutStream,MaxBasesDownstream),
-		process_stream(N1,InStream,OutStream,MaxBasesDownstream)).
+		process_orf_term(OrfTerm,OutStream),
+		process_stream(N1,InStream,OutStream)).
 	
 % Skip ORFs with more than one in frame amber codon
 process_orf_term(Orf,_) :-
@@ -27,34 +29,27 @@ process_orf_term(Orf,_) :-
    	NumStops > 1,
 	!.
 	
-
-/*
-process_orf_term(Orf,_OutStream,MaxBasesDownstream) :-
+process_orf_term(Orf,_OutStream) :-
 	gene_extra_field(Orf,in_frame_stops,[InFrameStop]),
 	gene_stop_codon(Orf,Stop),
 	distance(InFrameStop,Stop,Distance),
-	Distance < MaxBasesDownstream,
+	pylis_size(PylisSize),
+	Distance < PylisSize,
 	!.
-*/
 	
-process_orf_term(Orf,OutStream,MaxBasesDownstream) :-
+process_orf_term(Orf,OutStream) :-
 	gene_extra_field(Orf,in_frame_stops,[InFrameStop]),
 	gene_extra_field(Orf,sequence,Sequence),
 	gene_start_codon(Orf,Start),
 	distance(Start,InFrameStop,PrefixSize),
 	PrefixSize1 is PrefixSize - 1, % adjust to get also the t of t,a,g
 	prefix(PrefixSize1,Sequence,_Prefix,Suffix),
-        length(Suffix,SuffixLength),
-        ((SuffixLength < MaxBasesDownstream) ->
-                UAG_Downstream = Suffix
-                ;
-	        prefix(PylisSize,Suffix,UAG_Downstream,_Rest),
-        ),
-	gene_add_extra_field(Orf,pylis,UAG_Downstream,UpdatedOrf),
+	pylis_size(PylisSize),
+	prefix(PylisSize,Suffix,PylisSeq,_Rest),
+	gene_add_extra_field(Orf,pylis,PylisSeq,UpdatedOrf),
 	writeq(OutStream,UpdatedOrf),
 	write(OutStream,'.\n').
 	
-
 prefix(0,S,[],S).
 
 prefix(N,[S|Ss],[S|Ps],Suffix) :-
