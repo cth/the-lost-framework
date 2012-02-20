28c978bad68dbec0093fced3001694b01a9db33d
Christian Theil Have <christiantheilhave@gmail.com>
2012-02-20 10:52:05 +0100

    lostscript binary

---
+++ install.sh
@@ -0,0 +1,3 @@
+INSTALL_IN=/usr/local/bin/
+
+ln -s $(pwd)/lostscript $INSTALL_IN/lostscript
--- run_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-script="$1"
-goal="$2"
-
-echo "[lost], ['$script'], run($goal)."
-prism -g "[lost], ['$script'], run($goal)."
-
