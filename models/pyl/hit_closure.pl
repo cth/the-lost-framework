3c97557593e77e0aafcac8c8c4e5826b0e460204
Christian Theil Have <christiantheilhave@gmail.com>
2012-03-21 15:06:27 +0100

    hit_closure (works)

--- models/pyl/hit_closure.pl
+++ models/pyl/hit_closure.pl
@@ -5,33 +5,40 @@ tlink(A,B) :-
 
 tlink(A,B) :-
 	link(B,A).
-	
+
 tlink(A,C) :-
-	link(A,B),
+	tlink(A,B),
 	tlink(B,C).
 
 link([OrganismA,LeftA,RightA,StrandA,FrameA],[OrganismB,LeftB,RightB,StrandB,FrameB]) :-
 	blast_match(OrganismAFullname,LeftA,RightA,StrandA,FrameA,ExtraA),
-	member(match_to(orf(OrganismB,LeftB,RightB,StrandB,FrameB,ExtraB)), ExtraA),
+	member(match_to(orf(OrganismB,LeftB,RightB,StrandB,FrameB,_ExtraB)), ExtraA),
 	extract_organism_name(OrganismAFullname,OrganismA).
 	
 clusters(Clusters) :-
-	findall(A,tlink(_,A),ClustersDup),
-	eliminate_duplicate(ClustersDup,Clusters).
-
-/*	
-hit_closure(HitsFile,ClosureFile) :-
-	[HitsFile], % Load the HitsFile into memory
+	findall(C,cluster(C),ClustersDup),
+	length(ClustersDup,DupLen),
+	writeln(duplen(DupLen)),
+	!,
+	eliminate_duplicate(ClustersDup,Clusters),
+	length(Clusters,CLen),
+	writeln(clusters(CLen)).
 	
-cluster_hits([Hit|HitRest],Clusters) :-
-	member(Cluster,Clusters),
-	match_to_hit(Hit,Cluster),
-
+cluster(SortC) :-
+	tlink(A,_),
+	findall(B,tlink(A,B),C1),
+	eliminate_duplicate(C1,C),
+	sort(C,SortC).
+	
+hit_closure(HitsFile,ClusterFile) :-
+	[HitsFile], % Load the HitsFile into memory
+	clusters(Clusters),
+	open(ClusterFile,write,ClusterStream),
+	forall(member(Cluster,Clusters), (writeq(ClusterStream,cluster(Cluster)),write(ClusterStream,'.\n'))),
+	close(ClusterStream).
 
 test :-
-	hit_closure('data10.pl').
-*/	
-
+	hit_closure('data100.pl','clusters.pl').
 
 extract_organism_name(SeqId,JustName) :-
 	atom_codes(SeqId,Codes),
